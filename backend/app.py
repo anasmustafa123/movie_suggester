abfadcb157fbb9e3e91f94c54a553dd058c25729
anasmuostafa <anasmuostafa@gmail.com>
2024-02-06 19:04:12 +0200

    f

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,38 @@
-from flask import Flask, jsonify, request
-from flask_executor import Executor
+from flask import Flask, render_template, jsonify, request
+from mycode.fetch_suggestions import getMovieSuggestion, getShowsSuggestions
 from mycode.getSimilar import getSimilarMovies, getSimilarShows
-
+from flask_cors import CORS
+from flask_executor import Executor
 app = Flask(__name__)
 executor = Executor(app)
+
+# Enable CORS for requests from 
+CORS(app, resources={r"/*": {"origins": "https://movie-suggester-dun.vercel.app"}})
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/api/autocomplete_movies', methods=['POST'])
+def get_movies_suggestions():
+    try:
+        rdata = request.get_json()
+        movie_title = rdata["title"]
+        print("Received query:", movie_title)
+        result = getMovieSuggestion(movie_title)
+        print("Generated suggestions:", result)
+        return jsonify(result)
+    except Exception as e:
+        print("Error:", e)
+        return jsonify([])
+
+
+@app.route('/api/autocomplete_shows', methods=['POST'])
+def get_shows_suggestions():
+    rdata = request.get_json()
+    show_title = rdata["title"]
+    result  = getShowsSuggestions(show_title)
+    return jsonify(result)
+
 
 @app.route('/api/similar_movies', methods=['POST'])
 def get_similar_movies():
@@ -37,5 +66,3 @@ def get_similar_shows():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
