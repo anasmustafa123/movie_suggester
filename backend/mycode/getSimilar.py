b8a5a7aa6fd6b30ffb26bc663c4821d57c2f2b5a
anasmuostafa <anasmuostafa@gmail.com>
2024-02-11 14:16:22 +0200

    fix a typo, add a default url for missing posters

--- backend/mycode/getSimilar.py
+++ backend/mycode/getSimilar.py
@@ -10,7 +10,10 @@ def getSimilarMovies(movie_title, n):
     movies_sorted = sorted(list(enumerate(movies_similarity[title_index])), reverse=True, key = lambda x: x[1])
     L = []
     for i in movies_sorted[1:n]:
-        L.append({'title': movies.iloc[i[0]].title,'id': int(movies.iloc[i[0]].id), "poster_path": "https://image.tmdb.org/t/p/w600_and_h900_bestv2"+movies.iloc[i[0]]["poster_path"]})
+        movieposterUrl = movies.iloc[i[0]]["poster_path"]
+        if not isinstance(movieposterUrl, str):
+            movieposterUrl = "/mmQBX8qeracKZ2uz9zth3L3WKEc.jpg"
+        L.append({'title': movies.iloc[i[0]].title,'id': int(movies.iloc[i[0]].id), "poster_path": "https://image.tmdb.org/t/p/w600_and_h900_bestv2"+movieposterUrl})
     return L
 
 def getSimilarShows(show_title, n):
@@ -22,5 +25,8 @@ def getSimilarShows(show_title, n):
     shows_sorted = sorted(list(enumerate(shows_similarity[title_index])), reverse=True, key = lambda x: x[1])
     L = []
     for i in shows_sorted[1:n]:
-        L.append({'title': shows.iloc[i[0]].title,'id': int(shows.iloc[i[0]].id), 'score': int(i[1]), "poster_path": "https://image.tmdb.org/t/p/w600_and_h900_bestv2"+shows.iloc[i[0]]["poster_path"]})
+        showPosterUrl = shows.iloc[i[0]]["poster_path"]
+        if not isinstance(showPosterUrl, str):
+            showPosterUrl = "/mmQBX8qeracKZ2uz9zth3L3WKEc.jpg"
+        L.append({'title': shows.iloc[i[0]].title,'id': int(shows.iloc[i[0]].id), 'score': int(i[1]), "poster_path": "https://image.tmdb.org/t/p/w600_and_h900_bestv2"+showPosterUrl})
     return L
